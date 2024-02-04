c0a0f547fb6eb8018efef40c636d248caf3ecf59
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-02-04 15:12:51 +0100

    page offres & avantages

--- vues/page-offres-avantages.php
+++ vues/page-offres-avantages.php
@@ -2,19 +2,129 @@
 
     <h1>NOS OFFRES & AVANTAGES</h1>
 
+    <div id="div-grille-tarifaire">
+        <h2>Grille Tarifaire</h2>
+        <div id="div-grille-tarifaire-cards">
+            <div class="tarif-card box">
+                <div class="tarif-card-title">
+                    PREMIUM FORM
+                </div>
+                <div class="tarif-card-liste">
+                    CDI<br>
+                    MUSCU/CARDIO<br>
+                    CROSS TRAINING
+                </div>
+                <div class="tarif-card-price">
+                  29,90 €
+                </div>
+            </div>
+
+            <div class="tarif-card box">
+                <div class="tarif-card-title">
+                    PRIME FORM
+                </div>
+                <div class="tarif-card-liste">
+                    CDI<br>
+                    COURS COLLECTIFS<br>
+                    MUSCU/CARDIO<br>
+                    CROSS TRAINING
+                </div>
+                <div class="tarif-card-price">
+                34,90 €
+                </div>
+            </div>
+
+            <div class="tarif-card box">
+                <div class="tarif-card-title">
+                    FAMILY FORM<br>
+                    ETUDIANTS
+                </div>
+                <div class="tarif-card-liste">
+                    CDI<br>
+                    MUSCU/CARDIO<br>
+                    CROSS TRAINING
+                </div>
+                <div class="tarif-card-price">
+                25 €
+                </div>
+            </div>
+
+            <div class="tarif-card box">
+                <div class="tarif-card-title">
+                    OPTION
+                </div>
+                <div class="tarif-card-liste">
+                    PROGRAMME PERSONNALISE
+                </div>
+                <div class="tarif-card-price">
+                5 € / MOIS
+                </div>
+            </div>
+
+            <div class="tarif-card box">
+                <div class="tarif-card-title">
+                    OPTION
+                </div>
+                <div class="tarif-card-liste">
+                    FORMULE COACHING PERSONNALISE
+                </div>
+                <div class="tarif-card-price">
+                55 € / MOIS
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="div-avantages">
+        <h2>Avantages Actiform</h2>
+        <div id="div-avantages-cards">
+            <div class="avantages-card">
+                Ouvert 7 Jours / 7 toute l’année                
+            </div>
+            <div class="avantages-card">
+                Coachs diplômés d’État
+            </div>
+            <div class="avantages-card">
+                18 activités sportives
+            </div>
+            <div class="avantages-card">
+                Accès illimité
+            </div>
+            <div class="avantages-card">
+                Cours collectifs
+            </div>
+            <div class="avantages-card">
+                Ambiance conviviale
+            </div>
+            <div class="avantages-card">
+                Vestiaires avec douches individuelles
+            </div>
+            <div class="avantages-card">
+                800m2 d’espace entièrement climatisé
+            </div>
+            <div class="avantages-card">
+                Matériel haut de gamme Matrix
+            </div>
+            <div class="avantages-card">
+                Suivi et conseils personnalisés
+            </div>
+            <div class="avantages-card">
+                Acc&egrave;s personne &agrave; mobilit&eacute; r&eacute;duite
+            </div>
+            <div class="avantages-card">
+                Wi-Fi gratuit
+            </div>
+            <div class="avantages-card">
+                Espace détente
+            </div>
+            <div class="avantages-card">
+                Parking gratuit
+            </div>
+        
+
+        </div>
+
+    </div>
     
-Ouvert 7 Jours / 7 toute l’année<br>
-Coachs diplômés d’État<br>
-17 activités sportives<br>
-Accès illimité<br>
-Suivi et conseils personnalisés<br>
-Cours collectifs<br>
-Ambiance conviviale<br>
-Vestiaires avec douches individuelles<br>
-800m2 d’espace entièrement climatisé<br>
-Matériel haut de gamme Matrix<br>
-Accès handicapé<br>
-Wi-Fi gratuit<br>
-Espace détente<br>
-Parking gratuit<br>
 </div>
--- vues/page-seance-decouverte.php
+++ vues/page-seance-decouverte.php
@@ -1,51 +1,6 @@
 <?php
-	if(isset($_POST['decouverte-form-flag']) && 'flag' == $_POST['decouverte-form-flag']) {
-		echo '<div id="toaster-decouverte-form" class="paragraphe-normal box relative"><div id="toaster-decouverte-form-cross" class="absolute" onclick="closeContactFormToaster()">X</div>Votre message a bien &eacute;t&eacute; envoy&eacute;.<br>Nous vous recontactons dès que possible.</div>';
-		
-		//$DESTINATAIRE = "r.durin@lacentraledefinancement.fr";
-		$DESTINATAIRE = "louis.jdlfitness@gmail.com";
-		$DESTINATAIRE_BCC = "fabien.macip@gmail.com";
-        $mail = $DESTINATAIRE;
-		
-
-        $prenom = $_POST['fsm-prenom'] ?? '';
-        $tel = $_POST['fsm-tel'] ?? '';
-
-		$message = "Demande de séance découverte depuis le site actiform-colombiers.fr\n\n";
-		$message .= "Prénom : ".$prenom."\n\n";
-		$message .= "Téléphone : ".$tel."\n\n";
-    
-        //ICI, AJOUTER fonction dans services/mailEngine.php pour createMail
-
-        $dest = $DESTINATAIRE;
-        $sujet = "Message depuis le site ACTIFORM-COLOMBIERS.FR de la part de ".$prenom.".";
-        $corp = "\n".$message."\n";
-
-        $fromOK = 'mail_php@actiform-colombiers.fr';
-
-        $headers  = array(
-
-            'MIME-Version' => '1.0',
-            'From' => $fromOK,
-            'Reply-To' => ''.$mail,
-            'Bcc' => $DESTINATAIRE_BCC,
-            'Content-Type' => ' text/plain; charset="utf-8"; DelSp="Yes"; format=flowed ; ',
-            'Content-Disposition' => ' inline',
-            'Content-Transfer-Encoding' => ' 7bit',
-            'X-Envelope-From' => ' <'.$mail.'>',
-            'X-Mailer' => 'PHP/'.phpversion()
-        );
-
-        if (mail($dest, $sujet, $corp, $headers)) {
-        //echo "Email envoyé avec succès à $dest ...";
-        } else {
-        //echo "Échec de l'envoi de l'email...";
-        }
-
-}
-
+require_once('toaster/toaster-decouverte.php');
 ?>
-
 
 
 <div id="decouverte-div">
@@ -87,7 +42,7 @@
                        </div>
 
                        <div>
-                            <input type="checkbox" name="fsm-conditions" id="fsm-conditions"
+                            <input type="checkbox" name="fsm-conditions" id="fsm-conditions" tabindex="3"
                             onclick="checkContactFormField('fsm-conditions')">
                             Mentions l&eacute;gales lues et accept&eacute;es. <span class="asterisque">*</span>
                        </div>
