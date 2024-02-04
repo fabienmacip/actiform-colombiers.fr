52e85a5ea54ceaad9c5dd97a077b076aa1d118b7
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-02-04 10:06:05 +0100

    activites cards avec texte

--- vues/page-accueil.php
+++ vues/page-accueil.php
@@ -34,24 +34,192 @@ ob_start();
       <h2>18 activit&eacute;s sportives</h2>
       <p>
         <div id="liste-activites-sportives">
-          <div style="background-image: url('img/activites/actiform-plaisir.jpg');">Acti Attack</div>
-          <div style="background-image: url('img/activites/actiform-body-sculpt.jpg');">Acti Combat</div>
-          <div style="background-image: url('img/activites/actiform-caf.jpg');">Acti Pump</div>
-          <div style="background-image: url('img/activites/actiform-plaisir.jpg');">Acti Ring</div>
-          <div style="background-image: url('img/activites/actiform-bike.jpg');">Bike</div>
-          <div style="background-image: url('img/activites/actiform-body-sculpt.jpg');">Body Sculpt</div>
-          <div style="background-image: url('img/activites/actiform-caf.jpg');">C.A.F.</div>
-          <div style="background-image: url('img/activites/actiform-cross-training.jpg');">Cross Training</div>
-          <div style="background-image: url('img/activites/actiform-plaisir.jpg');">Cross Training HIIT</div>
-          <div style="background-image: url('img/activites/actiform-full-body.jpg');">Full Body</div>
-          <div style="background-image: url('img/activites/actiform-caf.jpg');">Gym Douce</div>
-          <div style="background-image: url('img/activites/actiform-cross-training.jpg');">HIIT-AF</div>
-          <div style="background-image: url('img/activites/actiform-pilates.jpg');">Pilates</div>
-          <div style="background-image: url('img/activites/actiform-plaisir.jpg');">Step</div>
-          <div style="background-image: url('img/activites/actiform-full-body.jpg');">Strech</div>
-          <div style="background-image: url('img/activites/actiform-bike.jpg');">Strong</div>
-          <div style="background-image: url('img/activites/actiform-pilates.jpg');">Swiss Ball</div>
-          <div style="background-image: url('img/activites/actiform-plaisir.jpg');">Zumba</div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-plaisir.jpg');" class="activite-img">Acti Attack</div>
+            <div class="activite-card-text">
+              <b>Acti Attack</b> révolutionne le fitness avec son approche <u>dynamique</u> et <u>intense</u>. 
+              Cette activité sportive combine cardio et renforcement musculaire pour un entraînement complet. 
+              Adaptée à tous les niveaux, elle promet des résultats rapides et une énergie décuplée. 
+              Prêt(e) à relever le défi d'Acti Attack pour <b>transformer votre condition physique</b> ?
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-body-sculpt.jpg');" class="activite-img">Acti Combat</div>
+            <div class="activite-card-text">
+              <b>Acti Combat</b> offre une expérience d'entraînement unique en fusionnant les <u>arts martiaux</u> et le <u>fitness</u>. 
+              Cette activité dynamique et intense combine des mouvements de combat avec des exercices cardiovasculaires, 
+              promettant un renforcement musculaire complet et une libération d'énergie. 
+              Prêt(e) à vous plonger dans <b>l'excitation d'Acti Combat</b> ?
+            </div>
+          </div>
+ 
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-caf.jpg');" class="activite-img">Acti Pump</div>
+            <div class="activite-card-text">
+              <b>Acti Pump</b> redéfinit l'entraînement de <u>renforcement musculaire</u>. 
+              Cette activité intense utilise des poids légers et répétitions rapides pour sculpter et 
+              tonifier l'ensemble du corps. Avec une musique entraînante et des mouvements synchronisés, 
+              Acti Pump offre une séance dynamique pour des résultats visibles. 
+              Prêt(e) à ressentir la <b>puissance d'Acti Pump</b> ?
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-plaisir.jpg');" class="activite-img">Acti Ring</div>
+            <div class="activite-card-text">
+              <b>Acti Ring</b> réinvente l'entraînement fonctionnel avec des <u>anneaux de résistance</u>. 
+              Cette activité novatrice combine renforcement musculaire, flexibilité et cardio, offrant 
+              une approche complète de la remise en forme. Les mouvements variés et ludiques d'Acti Ring 
+              promettent des résultats rapides et une expérience sportive engageante. 
+              Prêt(e) à <b>repousser vos limites avec Acti Ring</b> ?
+            </div>
+          </div>
+          
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-bike.jpg');" class="activite-img">Bike</div>
+            <div class="activite-card-text">
+              <b>Bike</b> est bien plus qu'un simple exercice cardiovasculaire. 
+              Cette activité de <u>cyclisme en salle</u> offre une expérience immersive, combinant musique motivante 
+              et entraînement intensif. Parfait pour brûler des calories, renforcer les jambes et améliorer 
+              l'endurance, le Bike transforme chaque séance en une <b>aventure énergisante</b>.
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-body-sculpt.jpg');" class="activite-img">Body Sculpt</div>
+            <div class="activite-card-text">
+              <b>Body Sculpt</b> redéfinit la remise en forme en concentrant son approche sur le <u>modelage du corps</u>. 
+              Cet entraînement tonique et ciblé combine exercices de renforcement musculaire et cardio 
+              pour sculpter une silhouette forte et définie. Prêt à <b>sculpter votre corps avec 
+              Body Sculpt</b> et à atteindre vos objectifs de remise en forme ?
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-caf.jpg');" class="activite-img">C.A.F.</div>
+            <div class="activite-card-text">
+              La discipline <b>Cuisse Abdos Fessiers (C.A.F.)</b> est une activité sportive ciblée visant le renforcement 
+              musculaire des <u>membres inférieurs</u> et de la <u>sangle abdominale</u>. À travers des exercices 
+              variés et ciblés, cette pratique favorise le tonus musculaire, la souplesse et la silhouette harmonieuse. 
+              Idéale pour <b>sculpter et raffermir le bas du corps</b>.
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-cross-training.jpg');" class="activite-img">Cross Training</div>
+            <div class="activite-card-text">
+              Le <b>Cross Training</b> offre une <u>polyvalence exceptionnelle</u> dans le monde du fitness. 
+              Cette discipline intense mêle divers exercices, tels que la musculation, l'endurance et 
+              la gymnastique, pour un entraînement complet. Idéale pour renforcer le corps de manière globale, 
+              le Cross Training promet <b>des résultats rapides et une condition physique optimale</b>.
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-plaisir.jpg');" class="activite-img">Cross Training HIIT</div>
+            <div class="activite-card-text">
+              Le <b>Cross Training HIIT</b> fusionne <u>l'intensité du High Intensity Interval Training</u> avec la 
+              <u>diversité du Cross Training</u>. Cet entraînement dynamique combine des exercices de force et cardio, 
+              optimisant la combustion des calories et le renforcement musculaire. 
+              Prêt(e) à relever le <b>défi du Cross Training HIIT pour des résultats explosifs</b> ?
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-full-body.jpg');" class="activite-img">Full Body</div>
+            <div class="activite-card-text">
+              L'activité <b>Full Body</b> révolutionne l'entraînement en <u>ciblant tous les groupes musculaires 
+              en une seule séance</u>. Avec des exercices polyvalents, cette pratique promet un renforcement 
+              musculaire global et une efficacité maximale. 
+              Le Full Body offre une solution complète pour <b>sculpter et tonifier votre corps en un temps record</b>.
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-caf.jpg');" class="activite-img">Gym Douce</div>
+            <div class="activite-card-text">
+              La <b>Gym Douce</b> privilégie une <u>approche en douceur</u> pour améliorer la condition physique. 
+              Axée sur la souplesse, le renforcement musculaire léger et la relaxation, cette activité 
+              favorise le bien-être physique et mental. Parfaite pour tous les niveaux, 
+              la Gym Douce offre une <b>expérience apaisante tout en maintenant la forme</b>.
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-cross-training.jpg');" class="activite-img">HIIT-AF</div>
+            <div class="activite-card-text">
+              Le <b>HIIT Abdos Fessiers</b> offre une <u>approche intense pour sculpter la silhouette</u>. 
+              Cette fusion de High Intensity Interval Training (HIIT) et de renforcement ciblé travaille 
+              spécifiquement les abdominaux et les fessiers. Des séances courtes mais intenses promettent 
+              des résultats rapides pour tonifier et raffermir ces zones clés du corps. 
+              Prêt(e) à <b>redéfinir vos abdos et fessiers</b> avec le HIIT Abdos Fessiers ?
+            </div>
+          </div>
+
+          <div class="activite-card">
+            <div style="background-image: url('img/activites/actiform-pilates.jpg');" class="activite-img">Pilates</div>
+            <div class="activite-card-text">
+              <b>Pilates</b> est une discipline axée sur la <u>force</u>, la <u>flexibilité</u> et le <u>contrôle corporel</u>. 
+              En se concentrant sur la respiration et des mouvements précis, cette pratique renforce 
+              les muscles profonds, améliore la posture et favorise la stabilité. 
+              Le Pilates offre une approche holistique pour <b>un corps fort et équilibré</b>.
+            </div>
+          </div>
+
+          <div class="activite-card">          
+            <div style="background-image: url('img/activites/actiform-plaisir.jpg');" class="activite-img">Step</div>
+            <div class="activite-card-text">
+              L'activité <b>Step</b> révolutionne le fitness avec l'utilisation d'une marche réglable. 
+              Cette séance cardio dynamique combine des mouvements simples mais efficaces pour <u>renforcer 
+              les jambes</u>, <u>améliorer la coordination</u> et <u>brûler des calories</u>. 
+              Le Step offre un <b>entraînement ludique et stimulant pour sculpter votre silhouette</b>. 
+              Prêt(e) à monter d'un cran avec la pratique du Step ?
+            </div>
+          </div>
+
+          <div class="activite-card">          
+            <div style="background-image: url('img/activites/actiform-full-body.jpg');" class="activite-img">Strech</div>
+            <div class="activite-card-text">
+              L'activité <b>Stretch</b> offre une <u>pause régénératrice</u> dans le monde du fitness. 
+              Axée sur l'étirement musculaire et la flexibilité, cette pratique favorise la détente 
+              et la mobilité. Idéale pour relâcher les tensions, l'activité Stretch améliore la posture et 
+              procure une sensation apaisante. <b>Un moment de bien-être pour votre corps et votre esprit</b>.
+            </div>
+          </div>
+
+          <div class="activite-card">          
+            <div style="background-image: url('img/activites/actiform-bike.jpg');" class="activite-img">Strong</div>
+            <div class="activite-card-text">
+              L'activité <b>Strong</b> redéfinit l'entraînement fonctionnel avec <u>une approche intense</u>. 
+              Cette activité polyvalente combine renforcement musculaire, cardio et entraînement par 
+              intervalles pour des résultats rapides. Conçu pour défier le corps et l'esprit, 
+              l'activité Strong promet une <b>condition physique optimale et une expérience sportive puissante</b>. 
+              Prêt(e) à devenir plus fort avec le Strong ?
+            </div>
+          </div>
+
+          <div class="activite-card">          
+            <div style="background-image: url('img/activites/actiform-pilates.jpg');" class="activite-img">Swiss Ball</div>
+            <div class="activite-card-text">
+              L'activité <b>Swiss Ball</b> intègre un <u>ballon de fitness pour un entraînement unique</u>. 
+              Ciblant l'équilibre, la stabilité et la force du corps, cette discipline engage les muscles profonds. 
+              Le Swiss Ball offre un exercice ludique, améliore la posture et renforce le corps de manière holistique. 
+              Prêt(e) à <b>rouler vers une meilleure forme</b> ?
+            </div>
+          </div>
+
+          <div class="activite-card">          
+            <div style="background-image: url('img/activites/actiform-plaisir.jpg');" class="activite-img">Zumba</div>
+            <div class="activite-card-text">
+              La <b>Zumba</b> transforme l'exercice en <u>une fête rythmée</u>. Cette activité de <u>danse cardio</u> combine 
+              des mouvements énergiques avec des rythmes entraînants pour créer un entraînement joyeux et efficace. 
+              La Zumba offre une expérience amusante tout en améliorant la forme physique, la coordination et le bien-être général. 
+              Prêt(e) à <b>danser vers une meilleure santé</b> ?
+            </div>
+          </div>
+
         </div>
       </p>
     </div>
