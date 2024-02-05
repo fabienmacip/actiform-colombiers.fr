a55309959579efda93aca9d18345ea44d2e4a2c9
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-02-05 13:16:39 +0100

    page planning et reservation - css modif

--- vues/page-planning-reservations.php
+++ vues/page-planning-reservations.php
@@ -52,11 +52,11 @@
     <div id="planning-grille">
         
         <div class="lun day">Lundi<!--<br><span>matin</span> --></div>
-        <div class="swiss-ball">9h30-10h<br>Swiss Ball</div>
-        <div class="acti-ring">10h-10h30<br>Acti Ring</div>
-        <div class="full-body">12h30-13h30<br>Full Body<br><br><br></div>
-        <div class="acti-pump2">18h-19h<br>Acti Pump</div>
-        <div class="zumba">
+        <div class="swiss-ball min30">9h30-10h<br>Swiss Ball</div>
+        <div class="acti-ring min30">10h-10h30<br>Acti Ring</div>
+        <div class="full-body min60">12h30-13h30<br>Full Body</div>
+        <div class="acti-pump2 min60">18h-19h<br>Acti Pump</div>
+        <div class="zumba min60">
             <div>19h-20h<br>Zumba</div>
             <div>19h-20h<br>Cross training HIIT</div>
             <div>
@@ -66,20 +66,20 @@
         </div>
         
         <div class="mar day">Mardi<!--<br><span>matin</span> --></div>
-        <div class="body-sculpt1">9h30-10h30<br>Body Sculpt</div>
-        <div class="pilates1">10h30-11h30<br>Pilates<br><br><br></div>
-        <div class="caf1">18h-19h<br>C.A.F.</div>
-        <div class="step">19h-20h<br>Step<br><br>19h-20h<br>Cross training</div>
+        <div class="body-sculpt1 min60">9h30-10h30<br>Body Sculpt</div>
+        <div class="pilates1 min60">10h30-11h30<br>Pilates</div>
+        <div class="caf1 min60">18h-19h<br>C.A.F.</div>
+        <div class="step min60">19h-20h<br>Step<br><br>19h-20h<br>Cross training</div>
         
         <div class="mer day">Mercredi<!--<br><span>matin</span> --></div>
-        <div class="hiit-af">12h30-13h30<br>HIIT-AF</div>
-        <div class="acti-pump3">18h-19h<br>Acti Pump</div>
-        <div class="strong">19h-20h<br>Strong<br><br>19h-20h<br>Cross training HIIT</div>
+        <div class="hiit-af min60">12h30-13h30<br>HIIT-AF</div>
+        <div class="acti-pump3 min60">18h-19h<br>Acti Pump</div>
+        <div class="strong min60">19h-20h<br>Strong<br><br>19h-20h<br>Cross training HIIT</div>
         
         <div class="jeu day">Jeudi<!--<br><span>matin</span> --></div>
-        <div class="acti-pump1">9h30-10h30<br>Acti Pump</div>
-        <div class="body-sculpt2">18h-18h45<br>Body Sculpt</div>
-        <div class="bike1">
+        <div class="acti-pump1 min60">9h30-10h30<br>Acti Pump</div>
+        <div class="body-sculpt2 min45">18h-18h45<br>Body Sculpt</div>
+        <div class="bike1 min105">
             <div class="bike1-pilates3">
                 <div>18h45-19h30<br>Bike</div>
                 <div>19h30-20h30<br>Pilates</div>
@@ -92,15 +92,15 @@
         </div>
         
         <div class="ven day">Vendredi<!--<br><span>matin</span> --></div>
-        <div class="gym-douce">9h30-10h30<br>Gym Douce</div>
-        <div class="caf2">18h-18h30<br>C.A.F.</div>
-        <div class="acti-attack">18h30-19h15<br>Acti Attack</div>
-        <div class="bike3">19h15-20h<br>Bike</div>
+        <div class="gym-douce min60">9h30-10h30<br>Gym Douce</div>
+        <div class="caf2 min30">18h-18h30<br>C.A.F.</div>
+        <div class="acti-attack min45">18h30-19h15<br>Acti Attack</div>
+        <div class="bike3 min45">19h15-20h<br>Bike</div>
         
         <div class="dim day">Dimanche<!--<br><span>matin</span> --></div>
-        <div class="acti-combat">10h-11h<br>Acti Combat</div>
-        <div class="pilates2">11h-12h<br>Pilates<br><br><br></div>
-        <div class="stretch1">12h-12h30<br>Stretch</div>
+        <div class="acti-combat min60">10h-11h<br>Acti Combat</div>
+        <div class="pilates2 min60">11h-12h<br>Pilates</div>
+        <div class="stretch1 min30">12h-12h30<br>Stretch</div>
         
         <div class="lun2 day day2">Lundi</div>
         <div class="mar2 day day2">Mardi</div>
