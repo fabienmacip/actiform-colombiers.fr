8b639322cc34702e73d49941a4673c8951f19160
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-02-05 13:06:44 +0100

    page planning et reservation

--- vues/page-planning-reservations.php
+++ vues/page-planning-reservations.php
@@ -1,5 +1,114 @@
 <div id="planning-reservations-div">
-
+    
     <h1>PLANNING & RESERVATIONS</h1>
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur similique distinctio quod quae quidem eligendi labore. Modi, fugiat. Voluptates nesciunt quibusdam totam illo ullam deserunt quam mollitia, repudiandae sequi veritatis officia tenetur modi vero, cum suscipit ab nisi possimus? Mollitia suscipit rem quas, labore, est quia inventore corrupti veritatis eos iusto eius, assumenda dolorum atque qui? Vitae qui eum illum voluptatem enim soluta ducimus adipisci reprehenderit aliquid temporibus dolore necessitatibus, aspernatur aut facere, rerum, eius minima corporis quas distinctio totam sapiente ab tenetur fugiat! Culpa doloremque, repudiandae earum maiores alias illo accusantium ullam inventore, provident quae ducimus. Tenetur eligendi minus distinctio accusamus? Cumque quod sint illum est minima. Eum ut nam dignissimos aperiam recusandae omnis nihil doloribus assumenda officia quasi, in odio saepe ea praesentium at fuga a minima deserunt dolorum inventore culpa possimus laudantium facilis qui. Numquam quia reiciendis quibusdam explicabo deleniti tempora nisi eum obcaecati in, repudiandae distinctio iusto est ipsum quos quas aperiam laboriosam. Corporis minus illo dignissimos libero fugit voluptatem harum at, velit cum distinctio ut facilis voluptas placeat ipsa eligendi, earum itaque. Voluptatibus aliquam eum cumque veritatis eius deleniti ad molestiae ut. Deleniti minus alias delectus assumenda unde, illo, autem corrupti velit laboriosam placeat iusto.
+    
+    <div id="resamania">
+        R&eacute;servez votre cours avec RESAMANIA.<br>
+        Cliquez sur le logo ci-dessous.<br><br>
+        <div class="box">
+            <a href="https://api.resamania.com/oauth/login/actiform?client_id=26_2532ba2d23446346e4f83dda1570fdd224ce70c546251c4ce84bd734e0e18811&redirect_uri=https://member.resamania.com/actiform/&response_type=code&back_username=1" 
+               target="_blank"
+            >
+                <img src="img/reserver/resalogo.png" alt="logo resamania">
+            </a>
+        </div>
+     
+<?php 
+/*    <form 
+        style="text-align: center;" 
+        action="https://actiform.resamania.fr/auth/PlainWithRedirect" 
+        method="post">
+        
+        <label>
+        <input 
+            style="width: 180px; height: 35px; border-radius: 5px; padding-left: 10px; font-size: 15px; margin-bottom: 0px;" 
+            name="user" 
+            type="text" 
+            placeholder="Identifiant">
+        </label>
+        <label>
+        <input 
+            style="width: 180px; height: 35px; border-radius: 5px; padding-left: 10px; font-size: 15px;" 
+            name="pass" 
+            type="password" 
+            placeholder="Mot de passe">
+        </label>
+        <label>
+        <input 
+            style="width: 155px; height: 35px; border-radius: 10px; font-size: 15px; margin-bottom: 0px; background: red; border: none; color: white;" 
+            type="submit" 
+            value="Se connecter">
+        </label>
+    </form> */
+?>
+    </div>
+
+<!-- **************************************************************************************
+**************************************************************************************
+************************************* P L A N N I N G ********************************
+**************************************************************************************
+************************************************************************************** -->
+
+    <div id="planning-grille">
+        
+        <div class="lun day">Lundi<!--<br><span>matin</span> --></div>
+        <div class="swiss-ball">9h30-10h<br>Swiss Ball</div>
+        <div class="acti-ring">10h-10h30<br>Acti Ring</div>
+        <div class="full-body">12h30-13h30<br>Full Body<br><br><br></div>
+        <div class="acti-pump2">18h-19h<br>Acti Pump</div>
+        <div class="zumba">
+            <div>19h-20h<br>Zumba</div>
+            <div>19h-20h<br>Cross training HIIT</div>
+            <div>
+                <div>19h-19h45<br>Bike</div>
+                <div></div>
+            </div>
+        </div>
+        
+        <div class="mar day">Mardi<!--<br><span>matin</span> --></div>
+        <div class="body-sculpt1">9h30-10h30<br>Body Sculpt</div>
+        <div class="pilates1">10h30-11h30<br>Pilates<br><br><br></div>
+        <div class="caf1">18h-19h<br>C.A.F.</div>
+        <div class="step">19h-20h<br>Step<br><br>19h-20h<br>Cross training</div>
+        
+        <div class="mer day">Mercredi<!--<br><span>matin</span> --></div>
+        <div class="hiit-af">12h30-13h30<br>HIIT-AF</div>
+        <div class="acti-pump3">18h-19h<br>Acti Pump</div>
+        <div class="strong">19h-20h<br>Strong<br><br>19h-20h<br>Cross training HIIT</div>
+        
+        <div class="jeu day">Jeudi<!--<br><span>matin</span> --></div>
+        <div class="acti-pump1">9h30-10h30<br>Acti Pump</div>
+        <div class="body-sculpt2">18h-18h45<br>Body Sculpt</div>
+        <div class="bike1">
+            <div class="bike1-pilates3">
+                <div>18h45-19h30<br>Bike</div>
+                <div>19h30-20h30<br>Pilates</div>
+            </div>
+            <div class="cross-training-chevauchant">
+                <div></div>
+                <div>19h-20h<br>Cross training</div>
+                <div></div>
+            </div>
+        </div>
+        
+        <div class="ven day">Vendredi<!--<br><span>matin</span> --></div>
+        <div class="gym-douce">9h30-10h30<br>Gym Douce</div>
+        <div class="caf2">18h-18h30<br>C.A.F.</div>
+        <div class="acti-attack">18h30-19h15<br>Acti Attack</div>
+        <div class="bike3">19h15-20h<br>Bike</div>
+        
+        <div class="dim day">Dimanche<!--<br><span>matin</span> --></div>
+        <div class="acti-combat">10h-11h<br>Acti Combat</div>
+        <div class="pilates2">11h-12h<br>Pilates<br><br><br></div>
+        <div class="stretch1">12h-12h30<br>Stretch</div>
+        
+        <div class="lun2 day day2">Lundi</div>
+        <div class="mar2 day day2">Mardi</div>
+        <div class="mer2 day day2">Mercredi</div>
+        <div class="jeu2 day day2">Jeudi</div>
+        <div class="ven2 day day2">Vendredi</div>
+
+    </div>
+
+
 </div>
