4f4249074c527bb0ec7076cf168e9bbe541f6857
fabienmacip <fabien.macip@gmail.com>
2025-08-31 15:35:40 +0200

    UPDATE Planning

---
+++ vues/page-planning-reservations copy.php
@@ -0,0 +1,180 @@
+<?php 
+/*    <form 
+        style="text-align: center;" 
+        action="https://actiform.resamania.fr/auth/PlainWithRedirect" 
+        method="post">
+        
+        <label>
+        <input 
+            style="width: 180px; height: 35px; border-radius: 5px; padding-left: 10px; font-size: 15px; margin-bottom: 0px;" 
+            name="user" 
+            type="text" 
+            placeholder="Identifiant">
+        </label>
+        <label>
+        <input 
+            style="width: 180px; height: 35px; border-radius: 5px; padding-left: 10px; font-size: 15px;" 
+            name="pass" 
+            type="password" 
+            placeholder="Mot de passe">
+        </label>
+        <label>
+        <input 
+            style="width: 155px; height: 35px; border-radius: 10px; font-size: 15px; margin-bottom: 0px; background: red; border: none; color: white;" 
+            type="submit" 
+            value="Se connecter">
+        </label>
+    </form> */
+?>
+
+
+<!-- DEBUT Planning 2024-2025 -->
+<!--
+<div id="planning-grille">
+        
+        <div class="lun day">Lundi</div>
+        <div class="swiss-ball min30">9h30-10h<br>Swiss Ball</div>
+        <div class="acti-ring min30">10h-10h30<br>Acti Ring</div>
+        <div class="full-body min45">12h40-13h20<br>HIIT Full Body</div>
+        <div class="acti-pump1 min60">18h-19h<br>Acti Pump</div>
+        <div class="zumba min60">
+            <div>19h-20h<br>Zumba</div>
+            <div>19h-20h<br>Cross training</div>
+            <div>
+                <div>19h-19h45<br>Bike</div>
+                <div></div>
+            </div>
+        </div>
+        
+        <div class="mar day">Mardi</div>
+        <div class="body-sculpt1 min60">9h30-10h30<br>Body Sculpt</div>
+        <div class="pilates1 min60">10h30-11h30<br>Pilates</div>
+        <div class="caf1 min30">18h-18h30<br>Circuit C.A.F.</div>
+        <div class="abdos min30">18h30-19h00<br>100 % Abdos</div>
+        <div class="step min60">
+            <div>
+                19h-20h<br>Step
+            </div>
+            <div>
+                19h-20h<br>Cross-Training
+            </div>
+        </div>
+        
+        <div class="mer day">Mercredi</div>
+        <div class="caf5 min30">9h30-10h30<br>C.A.F.</div>
+        <div class="hiit-af min60">12h30-13h30<br>13h-13h30<br>HIIT / AF</div>
+        <div class="acti-pump2 min60">18h-19h<br>Acti Pump</div>
+        <div class="strong-cross-training trois-quarts-et-soixante min60">
+            <div>
+                <div>19h-19h45<br>Strong</div>
+                <div></div>
+            </div>
+            <div>19h-20h<br>Cross training</div>
+        </div>
+        
+        <div class="jeu day">Jeudi</div>
+        <div class="acti-pump3 min60">9h30-10h30<br>Acti Pump</div>
+        <div class="body-sculpt2 min60">18h-18h45<br>Body Sculpt</div>
+        <div class="bike1 min105">
+            <div class="bike1-pilates3">
+                <div>18h45-19h30<br>Bike</div>
+                <div>19h30-20h15<br>Body Zen</div>
+            </div>
+            <div class="cross-training-chevauchant">
+                <div></div>
+                <div>19h-20h<br>Cross training</div>
+                <div></div>
+            </div>
+        </div>
+        
+        <div class="ven day">Vendredi</div>
+        <div class="gym-tonic1 min30">9h30-10h00<br>Gym Tonic</div>
+        <div class="stretch5 min30">10h00-10h30<br>Stretch</div>
+        <div class="caf2 min30">18h-18h30<br>C.A.F.</div>
+        <div class="acti-attack min45">18h30-19h15<br>Acti Attack</div>
+        <div class="bike3 trois-quarts-et-soixante min60">
+            <div>
+                <div>19h15-20h<br>Bike</div>
+                <div></div>
+            </div>
+            <div>19h15-20h15<br>Zumba</div>
+        </div>
+        
+        <div class="dim day">Dimanche</div>
+        <div class="acti-combat min60">10h-11h<br>Acti Combat</div>
+        <div class="pilates2 min60">11h-12h<br>Pilates</div>
+        <div class="stretch1 min30">12h-12h30<br>Stretch</div>
+        
+        <div class="lun2 day day2">Lundi</div>
+        <div class="mar2 day day2">Mardi</div>
+        <div class="mer2 day day2">Mercredi</div>
+        <div class="jeu2 day day2">Jeudi</div>
+        <div class="ven2 day day2">Vendredi</div>
+
+    </div> --><!-- FIN Planning 2024-2025 -->
+
+    <!-- DEBUT Planning 2023-2024 -->
+    <!--
+    <div id="planning-grille">
+        
+        <div class="lun day">Lundi</div>
+        <div class="swiss-ball min30">9h30-10h<br>Swiss Ball</div>
+        <div class="acti-ring min30">10h-10h30<br>Acti Ring</div>
+        <div class="full-body min60">12h30-13h30<br>Full Body</div>
+        <div class="acti-pump2 min60">18h-19h<br>Acti Pump</div>
+        <div class="zumba min60">
+            <div>19h-20h<br>Zumba</div>
+            <div>19h-20h<br>Cross training HIIT</div>
+            <div>
+                <div>19h-19h45<br>Bike</div>
+                <div></div>
+            </div>
+        </div>
+        
+        <div class="mar day">Mardi</div>
+        <div class="body-sculpt1 min60">9h30-10h30<br>Body Sculpt</div>
+        <div class="pilates1 min60">10h30-11h30<br>Pilates</div>
+        <div class="caf1 min60">18h-19h<br>C.A.F.</div>
+        <div class="step min60">19h-20h<br>Step<br><br>19h-20h<br>Cross training</div>
+        
+        <div class="mer day">Mercredi</div>
+        <div class="hiit-af min60">12h30-13h30<br>HIIT-AF</div>
+        <div class="acti-pump3 min60">18h-19h<br>Acti Pump</div>
+        <div class="strong min60">19h-20h<br>Strong<br><br>19h-20h<br>Cross training HIIT</div>
+        
+        <div class="jeu day">Jeudi</div>
+        <div class="acti-pump1 min60">9h30-10h30<br>Acti Pump</div>
+        <div class="body-sculpt2 min45">18h-18h45<br>Body Sculpt</div>
+        <div class="bike1 min105">
+            <div class="bike1-pilates3">
+                <div>18h45-19h30<br>Bike</div>
+                <div>19h30-20h30<br>Pilates</div>
+            </div>
+            <div class="cross-training-chevauchant">
+                <div></div>
+                <div>19h-20h<br>Cross training</div>
+                <div></div>
+            </div>
+        </div>
+        
+        <div class="ven day">Vendredi</div>
+        <div class="gym-douce min60">9h30-10h30<br>Gym Douce</div>
+        <div class="caf2 min30">18h-18h30<br>C.A.F.</div>
+        <div class="acti-attack min45">18h30-19h15<br>Acti Attack</div>
+        <div class="bike3 min45">19h15-20h<br>Bike</div>
+        
+        <div class="dim day">Dimanche</div>
+        <div class="acti-combat min60">10h-11h<br>Acti Combat</div>
+        <div class="pilates2 min60">11h-12h<br>Pilates</div>
+        <div class="stretch1 min30">12h-12h30<br>Stretch</div>
+        
+        <div class="lun2 day day2">Lundi</div>
+        <div class="mar2 day day2">Mardi</div>
+        <div class="mer2 day day2">Mercredi</div>
+        <div class="jeu2 day day2">Jeudi</div>
+        <div class="ven2 day day2">Vendredi</div>
+
+    </div>-->
+    <!-- FIN Planning 2023-2024 -->
+
+
